7bcfc1024e8b1266c8df77e32cde5de0a7f4b8df
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2017-08-10 20:11:18 +0200

    Componente Header

---
+++ demo/src/components/app-header/app-header.js
@@ -0,0 +1,8 @@
+import AppMenu from '@/components/commons/app-menu/app-menu.vue';
+
+export default {
+    name: 'app-header',
+    components: {
+        AppMenu
+    }
+};
---
+++ demo/src/components/app-header/app-header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="app-header">
+        <h1>Codenares Cinema Paradiso</h1>
+        <app-menu></app-menu>
+    </header>
+</template>
+<script src="./app-header.js"></script>
+<style  src="./app-header.scss" lang="scss" scoped></style>
+
+
