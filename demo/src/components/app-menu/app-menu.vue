07de4b4a91cdcdc33265d292402e19fa84fddd58
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2017-08-10 20:11:40 +0200

    Componente Menu

---
+++ demo/src/components/app-menu/app-menu.vue
@@ -0,0 +1,9 @@
+<template>
+    <ul>
+        <li>Home</li>
+        <li>Acerca de</li>
+    </ul>
+</template>
+<script src="./app-menu.js"></script>
+<style  src="./app-menu.scss" lang="scss" scoped></style>
+
