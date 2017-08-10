2aa22b58bcba3dffcdd5782daea457cd239aa127
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2017-08-10 18:33:17 +0200

    Proyecto Vue a mi gusto

---
+++ demo/src/components/layout/app/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div id="app">
+		<img src="../../../assets/logo.png">
+		<router-view></router-view>
+	</div>
+</template>
+
+<script src="./script.js"></script>
+<style  src="./style.scss" lang="scss"></style>
